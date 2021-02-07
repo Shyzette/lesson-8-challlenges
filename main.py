d63e29c5c32e63418bca7a841728f61b28b2df71
Shyzette <72762164+Shyzette@users.noreply.github.com>
2021-02-07 15:48:23 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,9 @@
+while True:
+    print(input.light_level()+372)
+    if input.light_level() > 500:
+        light.set_all(light.rgb(0,0,255))
+
+        
+    
+    
+    
---
+++ main.ts
@@ -0,0 +1,7 @@
+while (true) {
+    console.log(input.lightLevel() + 372)
+    if (input.lightLevel() > 500) {
+        light.setAll(light.rgb(0, 0, 255))
+    }
+    
+}
